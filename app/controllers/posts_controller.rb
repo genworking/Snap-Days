dc48ce0af28a4a8a25e66ca2e3d088ee21e4d01e
g.mita <g.mita0330@gmail.com>
2021-01-07 00:30:54 +0900

    add posts destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-
   before_action :authenticate_user!
+  before_action :set_post, only: %i(show destroy)
 
   def new
     @post = Post.new
@@ -19,16 +19,30 @@ class PostsController < ApplicationController
     end
   end
 
-  # ==========ここから追加する==========
   def index
     @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
   end
-  # ==========ここまで追加する==========
+
+  def show
+  end
+
+  # @post = Post.find_by(id: params[:id]) を削除する
+  def destroy
+    if @post.user == current_user
+      flash[:notice] = "投稿が削除されました" if @post.destroy
+    else
+      flash[:alert] = "投稿の削除に失敗しました"
+    end
+    redirect_to root_path
+  end
 
   private
-
     def post_params
       params.require(:post).permit(:caption, photos_attributes: [:image]).merge(user_id: current_user.id)
     end
 
+    # set_postというメソッドを追加する
+    def set_post
+      @post = Post.find_by(id: params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { registrations: 'registrations' }
 
-  root 'pages#home'
+  root 'posts#index'
 
   get '/users/:id', to: 'users#show', as: 'user'
 
   # この行を編集する
-  resources :posts, only: %i(new create index) do
+  resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
   end
 end
