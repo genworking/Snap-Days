0423bb208b2a9aed0ace40d42e249ea3f8ff2cf7
g.mita <g.mita0330@gmail.com>
2021-01-06 23:55:56 +0900

    add users edit page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
 
-  # この1行を追加
   validates :name, presence: true, length: { maximum: 50 }
+
+  # ==========ここから追加する==========
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+  # ==========ここまで追加する==========
+
 end
