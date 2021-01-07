18afd0d7f8929ee892362f0df8f73e3249a0e24d
g.mita <g.mita0330@gmail.com>
2021-01-07 15:41:05 +0900

    inplement follow & follwer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
 
-  # ==========ここから追加する==========
   def show
     @user = User.find_by(id: params[:id])
   end
-  # ==========ここまで追加する==========
+
+  def following
+      @user  = User.find(params[:id])
+      @users = @user.followings
+      render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes
   has_many :comments
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
@@ -11,7 +15,20 @@ class User < ApplicationRecord
 
   # mount_uploader :profile_photo, ProfilePhotoUploader
 
+  # ユーザーをフォローする
+  def following?(other_user)
+    following_relationships.find_by(following_id: other_user.id)
+  end
 
+  # ユーザーがフォローしているか調べる
+  def follow!(other_user)
+    following_relationships.create!(following_id: other_user.id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow!(other_user)
+    following_relationships.find_by(following_id: other_user.id).destroy
+  end
 
   # パスワードなしユーザー編集可能
   def update_without_current_password(params, *options)
