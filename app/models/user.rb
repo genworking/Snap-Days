549ce3239e56b9e75bd97ccf99b4efc83bd996b8
g.mita <g.mita0330@gmail.com>
2021-01-08 12:50:16 +0900

    add has_many :likes, dependent: :destroy for ForeignKeyViolation: ERROR

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :comments
   has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
   has_many :followings, through: :following_relationships
