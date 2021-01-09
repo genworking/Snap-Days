c82e8a774bd387d2216216d9869bd2bb48f38d7e
g.mita <g.mita0330@gmail.com>
2021-01-09 10:51:37 +0900

    fix avatar url retry

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def avatar_url(user)
     return user.profile_photo unless user.profile_photo.nil?
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "https://raw.githubusercontent.com/genworking/insta_clone5/develop/app/javascript/images/gravatar-icon.jpg?token=APZMHEWJHTBQMDX2NDWSGSC77B7PQ"
+    "https://raw.githubusercontent.com/genworking/insta_clone5/develop/app/javascript/images/gravatar-icon.jpg?token=APZMHEUSHDRXJSAQZ7KZUHC77EF4Q"
   end
 
 end
