e182b7da18e64e1323e4c3f3d85ccbcbec5bffca
g.mita <g.mita0330@gmail.com>
2021-01-09 11:37:51 +0900

    add gravatar icon png

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,7 @@ module ApplicationHelper
 
   def avatar_url(user)
     return user.profile_photo unless user.profile_photo.nil?
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "~gravatar-icon.png"
-
+    "~app/javascript/images/gravatar-icon.png"
   end
 
 end
