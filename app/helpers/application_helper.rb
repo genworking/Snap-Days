77848e945bf91f2dd8b6fb879598abc48ab41cfa
g.mita <g.mita0330@gmail.com>
2021-01-09 11:42:33 +0900

    fix avatar url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
 
   def avatar_url(user)
     return user.profile_photo unless user.profile_photo.nil?
-    "~app/javascript/images/gravatar-icon.png"
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://ja.gravatar.com/userimage/174693476/8c6a2b179a98780c3c83a2501d773cd9.jpg"
   end
 
 end
