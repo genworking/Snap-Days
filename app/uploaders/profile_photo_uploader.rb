ead8b1857698dd8055917e8907a275711cbbd603
g.mita <g.mita0330@gmail.com>
2021-02-05 16:40:01 +0900

    delete commentout - profile_photo_uploader.rb

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -7,18 +7,12 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # アップロードされたファイルがない場合に表示するデフォルトの画像の指定
-  # app/assets/images/fallback/default_user_avatar.png にファイルを置く
-  # def default_url(*args)
-  #   ActionController::Base.helpers.asset_path("https://ja.gravatar.com/userimage/174693476/8c6a2b179a98780c3c83a2501d773cd9.jpg")
-  # end
-
   # 画像の上限
   version :medium do
     process resize_to_fill: [300, 300]
   end
 
-  # サムネイルを生成する設定
+  # サムネイル生成設定
   version :thumb do
     process :resize_to_limit => [300, 300]
   end
@@ -43,10 +37,4 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   def filename
     super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
   end
-
-  # # ファイル名を日付にするとタイミングのせいでサムネイル名がずれる
-  # #ファイル名はランダムで一意になる
-  # def filename
-  #   "#{secure_token}.#{file.extension}" if original_filename.present?
-  # end
 end
