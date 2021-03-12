9c6f309946f4a099f911d6b2ac03233d86aae3ee
g.mita <g.mita0330@gmail.com>
2021-03-12 17:29:55 +0900

    使いやすくするためナビバーのマイページアイコンをプロフィール画像に変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
 
   def avatar_url(user)
     return user.profile_photo.url unless user.profile_photo.url.nil?
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "https://ja.gravatar.com/userimage/174693476/8c6a2b179a98780c3c83a2501d773cd9.jpg"
+    "app/javascript/images/gravatar-icon.png"
   end
 end
