3196b0db42a1f0d49843850fcdaf940c8507efc3
g.mita <g.mita0330@gmail.com>
2021-03-14 09:12:50 +0900

    見やすくするため通知アイコンの大きさと位置変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
 
   def avatar_url(user)
     return user.profile_photo.url unless user.profile_photo.url.nil?
-    "app/javascript/images/gravatar-icon.png"
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://ja.gravatar.com/userimage/174693476/8c6a2b179a98780c3c83a2501d773cd9.jpg"
   end
 end
