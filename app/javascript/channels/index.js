69a7890c67751bd1ac9fae7fbf2cd90df43b7769
g.mita <g.mita0330@gmail.com>
2021-03-15 22:22:03 +0900

    見やすくするため、ボタンデザイン変更、プレビュー削除ボタン連動

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,39 +4,50 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
+/* アイコン名表示 */
+$(function(){
+  $("input[type='file']").on('change',function(){
+     var file = $(this).prop('files')[0];
+     if(!($(".filename").length)){
+       $("#icon-name").append('<span class="filename"></span>');
+     }
+     $("p").addClass('changed');// CSSで中身を空欄に
+     $(".filename").html(file.name);// 空欄に入力ファイル名を出力
+   });
+ });
+
 /* アイコンプレビュー表示 */
 $(function() {
   function readURL(input) {
     if (input.files && input.files[0]) {
       var reader = new FileReader();
       reader.onload = function (e) {
-        $('#img_prev').attr('src', e.target.result);
+        $('#img_prev').attr('src', e.target.result);// プレビュー表示先
       }
       reader.readAsDataURL(input.files[0]);
     }
   }
-  // 画像入力フォームが変更されたら発火
-  $("#profile_photo").change(function(){
+  $("#input_file").change(function(){
     readURL(this);
   });
 });
 /* アイコンプレビュー削除 */
 $(function(){
   $('#preview-delete').click(function(){
-    $('input[type="file"]').val(null);
-    var src = $('.edit-profile-icon').attr('src');
-    $('.edit-profile-icon-preview').attr('src',src);
+    $('input[type="file"]').val(null);// file_fieldをクリア
+    var src = $('.edit-profile-icon').attr('src');// srcを定義し、
+    $('.edit-profile-icon-preview').attr('src',src);// 表示先にsrcを読み込ませる
+    $(".filename").html("選択されていません");// ファイル名表示をこの文字列に変更
     return false;
   });
 });
-
 
 /* profilebar */
 // リスト表示切り替え
 $(function() {
   $('.pb-btn').click(function() {
     $('.active').removeClass('active');
-   var clickedIndex = $('.pb-btn').index($(this));
+    var clickedIndex = $('.pb-btn').index($(this));
     $('.slide').eq(clickedIndex).addClass('active');
   });
 });
