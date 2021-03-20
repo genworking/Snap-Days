4c73ac1375f97a0071725b68ea1d329d62c84120
g.mita <g.mita0330@gmail.com>
2021-03-20 11:17:39 +0900

    分かりやすくするため、検索結果ないときflash追加

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,10 @@ class SearchController < ApplicationController
     if @hashtag.present?
       @hashtag_posts = @hashtag.posts.page(params[:page]).per(21).reverse_order
       @hashtags = Hashtag.all.to_a.group_by{ |hashtag| hashtag.posts.count}
-    else
-      
+    end
+    if @user.blank? && @hashtag_posts.blank?
+      redirect_to root_path(current_user)
+      flash[:alert] = "検索結果が見つかりませんでした"
     end
   end
 end
