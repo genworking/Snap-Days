cca07b8672b5382c6d19d8a0c5636e10c27cac68
g.mita <g.mita0330@gmail.com>
2021-03-26 17:41:25 +0900

    見やすくするため、search結果の表示制限数解除

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,14 +3,14 @@ class SearchController < ApplicationController
     @user = User.find_by('name LIKE(?) or username LIKE(?)', "%#{params[:search]}%","%#{params[:search]}%")
     if @user.present?
       @posts = @user.posts
-      @post_list = @user.posts.page(params[:page]).per(21).order('updated_at DESC')
+      @post_list = @user.posts.page(params[:page]).order('updated_at DESC')
       favorites = Favorite.where(user_id: current_user.id).pluck(:post_id)
       @favorite_list = Post.find(favorites)
-      @favorite_list = Kaminari.paginate_array(@favorite_list).page(params[:page]).per(21)
+      @favorite_list = Kaminari.paginate_array(@favorite_list).page(params[:page])
     end
     @hashtag = params[:search].try(:include?, "#") ? Hashtag.find_by(hashname: params[:search].delete('#')) : Hashtag.find_by(hashname: params[:search])
     if @hashtag.present?
-      @hashtag_posts = @hashtag.posts.page(params[:page]).per(21).reverse_order
+      @hashtag_posts = @hashtag.posts.page(params[:page]).reverse_order
       @hashtags = Hashtag.all.to_a.group_by{ |hashtag| hashtag.posts.count}
       @hashtag_randoms = @hashtag.posts.order("RANDOM()").limit(1)
     elsif @user.blank? && @hashtag_posts.blank?
