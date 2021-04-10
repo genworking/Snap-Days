0182f900092c402f57149789025957fb4e56c1de
g.mita <g.mita0330@gmail.com>
2021-04-11 01:03:37 +0900

    「他のユーザー」に本人が含まれていたため、変数修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
   def index
-    @current_user = current_user
     users = User.where.not(id: current_user.id).pluck(:id)# 自分以外のユーザー
     @users = User.find(users)# フォロー0人ページ用
-    @following_user = @current_user.followings.page(params[:page]).order(updated_at: :desc)# フォロー中ユーザー
-    other_unfollowed_users = Relationship.where.not(following_id: current_user.followings)
-                                         .pluck(:following_id)# 他のフォローしていないユーザー
+    @following_user = current_user.followings.page(params[:page]).order(updated_at: :desc)# フォロー中ユーザー
+    other_unfollowed_users = User.where.not(id: current_user.followings)
+                                 .where.not(id: current_user.id)
+                                 .pluck(:id)# 他のフォローしていないユーザー
     @other_unfollowed_user = User.find(other_unfollowed_users)# フォロー1人以上ページ用
   end
 
