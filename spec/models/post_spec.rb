733c19292a79b52b4d8174418a6ba5a41f513d52
g.mita <g.mita0330@gmail.com>
2021-04-25 17:51:17 +0900

    仕様確認のため、postモデルバリデーションテスト追記

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -15,8 +15,31 @@ RSpec.describe Post, type: :model do
       )
       expect(@post).to be_valid
     end
+
+    it 'captionが空だとNG' do
+      @post = Post.new(caption: '')
+      expect(@post.valid?).to eq(false)
+    end
+
+    it 'captionが2200文字を超えるとNG' do
+      @post = Post.new(caption: 'a' * 2201)
+      expect(@post.valid?).to eq(false)
+    end
+
+    it 'hashwordが136文字を超えるとNG' do
+      @post = Post.new(
+        caption: 'test caption',
+        hashword: 'a' * 137
+      )
+      expect(@post.valid?).to eq(false)
+    end
+
+    it 'addressが92文字を超えるとNG' do
+      @post = Post.new(
+        caption: 'test caption',
+        address: 'a' * 93
+      )
+      expect(@post.valid?).to eq(false)
+    end
   end
-
-  
-
 end
