11d38fe851287790680cddab47410f3d149e5e88
g.mita <g.mita0330@gmail.com>
2021-04-27 23:21:36 +0900

    境界値をテストするため、postテスト追記

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,25 +16,52 @@ RSpec.describe Post, type: :model do
       expect(@post.valid?).to eq(false)
     end
 
-    it 'captionが2200文字を超えるとNG' do
-      @post = Post.new(caption: 'a' * 2201)
-      expect(@post.valid?).to eq(false)
+    context 'captionの文字数制限' do
+      it 'captionが2200文字を超えるとNG' do
+        @post = Post.new(caption: 'a' * 2201)
+        expect(@post.valid?).to eq(false)
+      end
+
+      it 'captionが2200文字以内であればOK' do
+        @post = Post.new(caption: 'a' * 2200)
+        expect(@post.valid?).to eq(true)
+      end
     end
 
-    it 'hashwordが136文字を超えるとNG' do
-      @post = Post.new(
-        caption: 'test caption',
-        hashword: 'a' * 137
-      )
-      expect(@post.valid?).to eq(false)
+    context 'hashwordの文字数制限' do
+      it 'hashwordが136文字を超えるとNG' do
+        @post = Post.new(
+          caption: 'test caption',
+          hashword: 'a' * 137
+        )
+        expect(@post.valid?).to eq(false)
+      end
+
+      it 'hashwordが136文字以内であればOK' do
+        @post = Post.new(
+          caption: 'test caption',
+          hashword: 'a' * 136
+        )
+        expect(@post.valid?).to eq(true)
+      end
     end
 
-    it 'addressが92文字を超えるとNG' do
-      @post = Post.new(
-        caption: 'test caption',
-        address: 'a' * 93
-      )
-      expect(@post.valid?).to eq(false)
+    context 'addressの文字数制限' do
+      it 'addressが92文字を超えるとNG' do
+        @post = Post.new(
+          caption: 'test caption',
+          address: 'a' * 93
+        )
+        expect(@post.valid?).to eq(false)
+      end
+
+      it 'addressが92文字以内であればOK' do
+        @post = Post.new(
+          caption: 'test caption',
+          address: 'a' * 92
+        )
+        expect(@post.valid?).to eq(true)
+      end
     end
   end
 end
