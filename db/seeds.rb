7cee81fcecfd27c41519754705686812fa7fd707
g.mita <g.mita0330@gmail.com>
2021-04-29 12:53:15 +0900

    開発しやすくするため、seedに固定ユーザーを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+# 固定ユーザー作成
+hobby = Faker::Book.title
+[
+  ['ふるねーむ', 'ユーザーネーム', 'test1@test.com', 'hogehoge', File.open('./app/assets/images/default-first-user-icon.png'),
+    'https://www.google.com/', "ユーザーネームです！<br />好きな本は、「#{hobby}」です！！<br />よろしくお願いします😌",
+    '00011112222', 0],
+  ['田中太郎', 'タナカタロウ', 'test2@test.com', 'gehogeho', File.open('./app/assets/images/default-second-user-icon.png'),
+    'https://www.yahoo.co.jp/', "タナカタロウです！<br />好きな本は、「#{hobby}」です！！<br />よろしくお願いします😌",
+    '00033334444', 0],
+  ['山田花子', 'ヤマダハナコ', 'test3@test.com', 'foobar', File.open('./app/assets/images/default-third-user-icon.png'),
+    'https://www.youtube.com/', "ヤマダハナコです！<br />好きな本は、「#{hobby}」です！！<br />よろしくお願いします😌",
+    '00055556666', 1]
+].each do |name, username, email, password, profile_photo,
+           website, introduction,
+           phone_number, sex|
+  User.create!(
+    { name: name, username: username, email: email, password: password, profile_photo: profile_photo,
+      website: website, introduction: introduction, phone_number: phone_number, sex: sex}
+  )
+end
+
 # サンプルユーザー作成
 50.times do |n|
   name = Faker::Name.name
