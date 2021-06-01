8315ac58858c493ba6c78c955c1a2a3129a87cbd
g.mita <g.mita0330@gmail.com>
2021-06-02 00:14:10 +0900

    機能確認のため、hashword入力検索テスト追加

--- spec/features/hashtags_spec.rb
+++ spec/features/hashtags_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Hashtags", type: :feature do
-  describe '検索機能' do
+  describe 'フォーム入力からの検索として' do
     scenario '名前を検索できること' do
       @user = FactoryBot.create(:user)
 
@@ -28,6 +28,26 @@ RSpec.feature "Hashtags", type: :feature do
       find('#search-btn').click
 
       expect(page).to have_content @user.username.to_s
+    end
+
+    scenario 'ハッシュタグを検索できること' do
+      @user = FactoryBot.create(:user)
+
+      visit new_user_session_path
+      fill_in "user[email]", with: @user.email
+      fill_in "user[password]", with: @user.password
+      click_button "commit"
+
+      visit new_post_path
+      attach_file "post[photos_attributes][0][image]", "#{Rails.root}/spec/factories/images/2.jpeg"
+      fill_in "post[caption]", with: "test_caption"
+      fill_in "post[hashword]", with: "test_hashword"
+      click_button "commit"
+
+      fill_in "search", with: @user.posts.first.hashword
+      find('#search-btn').click
+
+      expect(page).to have_content @user.posts.first.hashword.to_s
     end
   end
 end
